ddf12edd376312bc49128b5ff7d6cf8067d3d4aa
Admin <jonathan.au@gmail.com>
2026-04-24 20:50:24 -0700

    chore: add SEO meta tags, structured data, favicon, robots.txt, sitemap, and web manifest

--- src/server.js
+++ src/server.js
@@ -14,7 +14,10 @@ const MIME_TYPES = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".svg": "image/svg+xml",
+  ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
 };
 
 export function createServer() {
